51209975cd34cec00c98b77a7b1e8ee960b70a4c
anilkumar222222 <anilkumar.vemu.cse@gmail.com>
2026-01-19 09:59:52 +0530

    new challenges added

---
+++ 10-java-core/challenge-03/SafeExecutor.java
@@ -0,0 +1,25 @@
+package Challenges;
+
+import java.util.Optional;
+
+public class SafeExecutor {
+
+    @FunctionalInterface
+    interface ThrowingSupplier<T> {
+        T get() throws Exception;
+    }
+
+    public static <T> Optional<T> safeExecute(ThrowingSupplier<T> supplier) {
+        try {
+            return Optional.ofNullable(supplier.get());
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(safeExecute(() -> Integer.parseInt("123")));
+        System.out.println(safeExecute(() -> Integer.parseInt("abc")));
+    }
+}
+
